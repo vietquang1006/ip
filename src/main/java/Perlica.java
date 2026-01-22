ee9b431e76b77ae3388e5ee309277d28f025717a
VQ <ngvietquang1006@gmail.com>
2026-01-23 02:52:38 +0800

    Level 1: Echo

--- src/main/java/Perlica.java
+++ src/main/java/Perlica.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
 public class Perlica {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Perlica");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        boolean isExit = false;
+        while (!isExit) {
+            String input = scanner.nextLine(); // read user input
+            if (input.equalsIgnoreCase("bye")) {
+                isExit = true;
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" " + input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+        scanner.close();
     }
 }
