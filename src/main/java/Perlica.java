187093b0bbecb2b428f2b2c3dc137266451415dd
VQ <ngvietquang1006@gmail.com>
2026-01-23 15:40:32 +0800

    Level 2. Add, List

--- src/main/java/Perlica.java
+++ src/main/java/Perlica.java
@@ -2,24 +2,36 @@ import java.util.Scanner;
 public class Perlica {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Perlica");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        boolean isExit = false;
-        while (!isExit) {
-            String input = scanner.nextLine(); // read user input
+        while (true) {
+            String input = scanner.nextLine().trim();
             if (input.equalsIgnoreCase("bye")) {
-                isExit = true;
                 System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
-            } else {
+                break;
+            } else if (input.equalsIgnoreCase("list")) {
                 System.out.println("____________________________________________________________");
-                System.out.println(" " + input);
+                if (taskCount == 0) {
+                    System.out.println(" No tasks yet.");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    }
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
                 System.out.println("____________________________________________________________");
             }
         }
-        scanner.close();
     }
 }
